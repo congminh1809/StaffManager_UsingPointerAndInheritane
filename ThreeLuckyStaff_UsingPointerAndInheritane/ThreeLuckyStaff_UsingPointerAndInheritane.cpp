5252af470ed2937ff0582717b20442b456594f68
congminh1809 <congminh1809@gmail.com>
2021-05-13 20:50:00 +0700

    Add project files.

---
+++ ThreeLuckyStaff_UsingPointerAndInheritane/ThreeLuckyStaff_UsingPointerAndInheritane.cpp
@@ -0,0 +1,36 @@
+﻿// ThreeLuckyStaff_UsingPointerAndInheritane.cpp 
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* KHAI BÁO CẤU TRÚC CLASS CHA - LỚP CƠ SỞ */
+class NhanVien
+{
+protected:
+    string hoten;
+    int tuoi;
+    string gioi_tinh;
+    string dia_chi;
+    string sdt;
+    float chieu_cao;
+    float can_nang;
+
+public:
+    void Nhap();
+    void Xuat();
+
+    NhanVien();
+    ~NhanVien();
+};
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
